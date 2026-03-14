bc3dcc07371f347049dfe747cf50e34269b29efa
Parth Agrawal <parthagrawal2904@gmail.com>
2026-03-14 10:04:14 +0530

    Add main application structure with components

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,1 +1,32 @@
+import BackgroundCanvas from './components/BackgroundCanvas';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Events from './components/Events';
+import Projects from './components/Projects';
+import { Team, Resources, Blog, Footer } from './components/StaticSections';
+import JoinForm from './components/JoinForm';
 
+function App() {
+  return (
+    <>
+      <BackgroundCanvas />
+      <Navbar />
+      <Hero />
+      <div className="divider"></div>
+      <Events />
+      <div className="divider"></div>
+      <Projects />
+      <div className="divider"></div>
+      <Team />
+      <div className="divider"></div>
+      <Resources />
+      <div className="divider"></div>
+      <Blog />
+      <div className="divider"></div>
+      <JoinForm />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
