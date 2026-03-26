866991310fca250d71f5ff4ce6991c6659d1185c
Parth Agrawal <parthagrawal2904@gmail.com>
2026-03-26 10:05:12 +0530

    Update API endpoint in JoinForm component

--- frontend/src/components/JoinForm.jsx
+++ frontend/src/components/JoinForm.jsx
@@ -26,7 +26,7 @@ export default function JoinForm() {
 
     try {
       // 1. The Fetch Request
-     const response = await fetch('https://club-website-7aay-avr701wve-parthagrawal-07s-projects.vercel.app/api/apply', {
+     const response = await fetch('https://club-website-7aay.vercel.app/api/apply', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json', // Tell the server we are sending JSON
